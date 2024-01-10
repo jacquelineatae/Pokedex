1405ed13a5d688f547601b33a250b1603f4c4225
jacquelineatae <dev.atae02@gmail.com>
2024-01-10 15:07:18 -0300

    Commenting the code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,21 @@
+// Get references to HTML elements
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton');
 const maxRecords = 151;
 const limit = 10;
 let offset = 0;
 
+// Function to toggle the display of a navigation menu
 function toggleMenu() {
     const navMenu = document.querySelector('.nav-menu');
     navMenu.style.display = (navMenu.style.display === 'block') ? 'none' : 'block';
 }
 
-
+// Function to load Pokémon items and update the HTML
 function loadPokemonItens(limit, offset) {
+    // Fetch Pokémon data using pokeApi.getPokemons() method
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        // Generate HTML for each Pokémon and append it to the pokemonList element
         const newHtml = pokemons.map((pokemon) => `
             <li class="pokemon ${pokemon.type}" data-pokemon-id="${pokemon.number}">
                 <span class="number">#${pokemon.number}</span>
@@ -26,7 +30,7 @@ function loadPokemonItens(limit, offset) {
         `).join('');
         pokemonList.innerHTML += newHtml;
 
-        // Adicione um event listener para cada elemento Pokemon
+        // Add click event listeners to each Pokémon for opening popups
         const pokemonElements = document.querySelectorAll('.pokemon');
         pokemonElements.forEach((element) => {
             element.addEventListener('click', () => {
@@ -37,78 +41,83 @@ function loadPokemonItens(limit, offset) {
     });
 }
 
+// Function to open a popup with detailed Pokémon information
 function openPopup(pokemonId) {
-    // Remova o popup anterior, se existir
+    // Remove any existing popup
     const previousPopup = document.querySelector('.popup-container');
     if (previousPopup) {
         previousPopup.remove();
     }
 
+    // Fetch detailed Pokémon data using pokeApi.getPokemonsDetail() method
     pokeApi.getPokemonsDetail({ url: `https://pokeapi.co/api/v2/pokemon/${pokemonId}/` })
         .then((pokemon) => {
+            // Generate HTML for the popup
             const popupContent = `
-            <div class="popup-container">
-            <button class="close-button">&times;</button>
-            <img src="${pokemon.photo}" alt="${pokemon.name}" class="popup-image">
-            <div class="popup-details">
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
-                <div class="detail">
-                    <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    </ol>
-                    <p><strong>Height:</strong> ${pokemon.height} dm</p>
-                    <p><strong>Weight:</strong> ${pokemon.weight} hectograms</p>
-                    <p><strong>Abilities:</strong> ${pokemon.abilities.join(', ')}</p>
+                <div class="popup-container custom-popup">
+                    <button class="close-button">&times;</button>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}" class="popup-image">
+                    <div class="popup-details">
+                        <span class="number">#${pokemon.number}</span>
+                        <span class="name">${pokemon.name}</span>
+                        <div class="detail">
+                            <ol class="types">
+                                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                            </ol>
+                            <p><strong>Height:</strong> ${pokemon.height} dm</p>
+                            <p><strong>Weight:</strong> ${pokemon.weight} hectograms</p>
+                            <p><strong>Abilities:</strong> ${pokemon.abilities.join(', ')}</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        `;
+            `;
 
-            // Adicione o conteúdo do popup ao body
+            // Insert the popup HTML into the document body
             document.body.insertAdjacentHTML('beforeend', popupContent);
 
-            // Adicione um event listener para fechar o popup ao clicar no botão de fechar
+            // Add click event listener to the close button
             const closeButton = document.querySelector('.close-button');
             if (closeButton) {
                 closeButton.addEventListener('click', (event) => {
-                    event.stopPropagation(); // Impede a propagação do evento para o body
+                    event.stopPropagation();
                     closePopup();
                 });
             }
 
-            // Adicione um event listener para fechar o popup ao clicar fora dele
+            // Add click event listener to close the popup when clicking outside it
             document.body.addEventListener('click', closePopup);
         })
         .catch((error) => console.error(error));
 }
 
-
-
+// Function to close the currently open popup
 function closePopup() {
     const popupContainer = document.querySelector('.popup-container');
     if (popupContainer) {
         popupContainer.remove();
-        // Remova o event listener após fechar o popup
         document.body.removeEventListener('click', closePopup);
     }
 }
 
-
-
+// Initial loading of Pokémon items
 loadPokemonItens(limit, offset);
 
+// Add click event listener to the "Load More" button
 loadMoreButton.addEventListener('click', () => {
+    // Update offset for the next batch of Pokémon
     offset += limit;
 
     const qtdRecordNextPage = offset + limit;
 
+    // Check if we have reached the maximum number of records
     if (qtdRecordNextPage >= maxRecords) {
+        // If reached, load the remaining Pokémon and remove the "Load More" button
         const newLimit = maxRecords - offset;
         loadPokemonItens(limit, newLimit);
 
         loadMoreButton.parentElement.removeChild(loadMoreButton);
     } else {
+        // If not reached, load the next batch of Pokémon
         loadPokemonItens(limit, offset);
     }
 });
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,41 +1,57 @@
+// Define an object to encapsulate Pokémon-related API functions
 const pokeApi = {}
 
-function convertPokeApiDetailPokemon(pokeDetail){
+// Function to convert PokeAPI details into a custom Pokemon object
+function convertPokeApiDetailPokemon(pokeDetail) {
+    // Create a new Pokemon object
     const pokemon = new Pokemon()
+
+    // Extract relevant information from the PokeAPI response
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
+    // Extract types and set the primary type
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
     pokemon.types = types
     pokemon.type = type
 
+    // Set the photo of the Pokemon
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
+    // Set height and weight
     pokemon.height = pokeDetail.height
     pokemon.weight = pokeDetail.weight
 
+    // Extract abilities
     pokemon.abilities = pokeDetail.abilities.map((abilitySlot) => abilitySlot.ability.name);
-    
 
     return pokemon
 }
 
+// Function to get detailed information about a specific Pokemon from the PokeAPI
 pokeApi.getPokemonsDetail = (pokemon) => {
+    // Fetch data for a specific Pokemon from the provided URL
     return fetch(pokemon.url)
         .then((response) => response.json())
         .then(convertPokeApiDetailPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit =10) => {
+// Function to get a list of Pokemon from the PokeAPI, with optional offset and limit parameters
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+    // Construct the URL for fetching a list of Pokemon
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-   
+
+    // Fetch the list of Pokemon from the PokeAPI
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
+        // Fetch detailed information for each Pokemon in the list
         .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetail))
+        // Wait for all detailed information requests to complete
         .then((detailRequests) => Promise.all(detailRequests))
+        // Return the array of detailed Pokemon information
         .then((PokemonsDetail) => PokemonsDetail)
         .catch((error) => console.error(error))
 }
